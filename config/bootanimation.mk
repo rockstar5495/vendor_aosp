2bad7b2955151642903d18a23d66b0507fc80624
Sushrut1101 <guptasushrut@gmail.com>
2022-01-23 01:16:10 +0530

    Add TARGET_CUSTOM_BOOTANIMATION Flag * Allows us to use a custom ZIP File as the Bootanimation ZIP. * If you're using this Flag, Make Sure that it's a valid Bootanimation ZIP. *NOT* a Flashable ZIP. * eg: TARGET_CUSTOM_BOOTANIMATION := $(DEVICE_PATH)/bootanimation/bootanimation.zip
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,16 +1,21 @@
-
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+ifdef TARGET_CUSTOM_BOOTANIMATION
+    PRODUCT_COPY_FILES += \
+        $(TARGET_CUSTOM_BOOTANIMATION):$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    $(warning "Using $(TARGET_CUSTOM_BOOTANIMATION) as the Bootanimation ZIP.")
 else
-    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+         PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+         PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+         PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
     else
-        $(warning "Current bootanimation res is not supported, forcing 1080p")
+        ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+            $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+        else
+            $(warning "Current bootanimation res is not supported, forcing 1080p")
+        endif
+        PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
     endif
-    PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
